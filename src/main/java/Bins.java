00124a1b09e6cb6f24ddef936c04f9cac2d107e2
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2024-11-12 23:51:23 -0500

    Completed Dice and Bin code

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,29 @@
+import java.util.HashMap;
 
 public class Bins {
+
+    HashMap<Integer, Integer> binMap = new HashMap<>();
+
+
+    Dice dice = new Dice(2);
+    Integer toss = dice.tossAndSum();
+
+
+    public Integer incrementBin(Integer toss) {
+
+        if(binMap.containsKey(toss)){
+           Integer existingVal = binMap.get(toss);
+           binMap.replace(toss,existingVal+1);
+        }
+        else {
+            binMap.put(toss,1);
+        }
+            return binMap.get(toss);
+    }
+
+    public Integer getBin(Integer tossValue){
+
+        return binMap.get(tossValue);
+    }
 
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,29 @@
+import java.util.Random;
+
 public class Dice {
 
+ int numOfDice = 0;
+
+    public Dice(int numOfDice) {
+        this.numOfDice = numOfDice;
+    }
+
+    public int getNumOfDice() {
+        return numOfDice;
+    }
+
+    public void setNumOfDice(int numOfDice) {
+        this.numOfDice = numOfDice;
+    }
+
+
+    public Integer tossAndSum(){
+        int dice1=(int)(Math.random()*6+1);
+        int dice2 = (int) (Math.random() *6+1);
+
+        int sum = dice1+dice2;
+
+        return sum;
+    }
 
 }
